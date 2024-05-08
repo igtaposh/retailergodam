bf8fad5ae8596d1828c9576fa1abb5abdd0cb531
igtaposh <debnathtaposh58@gamil>
2024-05-08 11:51:30 +0530

    We've made Page 1 responsive for phones and added two options when you click the create button.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -8,3 +8,32 @@ setting.addEventListener('click', () => {
 back.addEventListener('click', () => {
     navbar.classList.add('navbar');
 });
+
+const optionSection = document.querySelector('.option-section');
+const createBtn = document.querySelector('.create-btn');
+const dshBtn = document.querySelector('.dashboad-btn');
+
+createBtn.addEventListener('click', () => {
+    createBtn.classList.toggle("bg-[#F1F1F1]");
+    createBtn.classList.toggle("scale-90");
+    createBtn.classList.toggle("hover:scale-95");
+    createBtn.classList.toggle("opacity-80");
+    dshBtn.classList.toggle("bg-[#F1F1F1]");
+    dshBtn.classList.toggle("scale-90");
+    dshBtn.classList.toggle("hover:scale-95");
+    dshBtn.classList.toggle("opacity-80");
+    optionSection.classList.toggle('hidden');
+
+    dshBtn.addEventListener("click", () => {
+        optionSection.classList.add('hidden');
+        createBtn.classList.remove("bg-[#F1F1F1]");
+        createBtn.classList.remove("scale-90");
+        createBtn.classList.add("hover:scale-95");
+        createBtn.classList.remove("opacity-80");
+        dshBtn.classList.add("bg-[#F1F1F1]");
+        dshBtn.classList.add("scale-90");
+        dshBtn.classList.remove("hover:scale-95");
+        dshBtn.classList.add("opacity-80");
+    })  
+})
+
